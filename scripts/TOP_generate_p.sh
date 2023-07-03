5fb5ee8685079bf642cff47c01e3efe3c6e1cfee
Michael Apostolides <michael.j.apostolides@gmail.com>
2023-07-03 16:30:59 -0400

    p matrix generation scripts point to repo code

--- scripts/TOP_generate_p.sh
+++ scripts/TOP_generate_p.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Set output directory
-topdir=
+topdir=/home/maposto/scratch/pmat_single.TEST_github.JULY-3-2023
 
 mode=single
 #mode=paired
 
 ref_txome=/project/6007998/maposto/reference/gencode.v38.transcripts.fa
+KALLISTO_IDX=/project/6007998/maposto/reference/kallisto/gencode.v38.transcripts.fa.idx
 
 # REPLICATES
-job1=$(sbatch --export=topdir=$topdir,mode=$mode replicates_combined_script.sh, --export=ref_txome=$ref_txome)
+job1=$(sbatch --export=topdir=$topdir,mode=$mode,ref_txome=$ref_txome,KALLISTO_IDX=$KALLISTO_IDX replicates_combined_script.sh)
 echo $job1
 job1=$(echo $job1 | awk '{print $NF}')
 
 # COMBINE
 job2=$(sbatch --account=rrg-hsn --dependency=afterany:$job1 --export=topdir=$topdir RUN_combine_replicate_counts.sh) 
-
-#job2=$(sbatch --account=rrg-hsn --export=topdir=$topdir RUN_combine_replicate_counts.sh) 
 echo $job2
