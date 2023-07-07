a8e36daa660f41949d9ec1d35414b3c98818a237
Michael Apostolides <maposto@Michaels-MacBook-Pro.local>
2023-07-07 14:18:00 -0400

    MPAQT.R mod

--- scripts/MPAQT.R
+++ scripts/MPAQT.R
@@ -316,16 +316,19 @@ n_sample[names(n)[which(! names(n) %in%  reads.ecs.counts$txs )]] <- 0
 # order n_sample vector by order of n from P
 n_sample <- n_sample[names(n)]
 
-tol=1e-4
+#tol=1e-4
+tol=1e-10
 itelim=100
 # Fit model with only SR
 #res <- fit_model( P, n_sample,  tol=tol, itelim=itelim )
 
+set.seed(789)
 print("FITTING MPAQT WITH SR DATA ONLY")
 res <- fit_model.v9.better_convergence( P, n_sample, prior=T, tol=tol, itelim=itelim )
 print("FITTING MPAQT WITH LR + SR DATA")
 res2 <- fit_model.v9.better_convergence( P, n_sample, n2=LR.counts,covMx = covMx, prior=T, tol=tol, itelim=itelim )
 #res <- readRDS(' fit.tol= 1e-04 .itelim= 100 .Rds') 
 #saveRDS(res, file=file.path(topdir, paste0("fit.tol=", tol, ".itelim=", itelim, ".Rds") ))
+print("SAVING Rds OBJECTS")
 saveRDS(res, file=file.path(topdir, "MPAQT_output.SR.Rds"))
 saveRDS(res2, file=file.path(topdir, "MPAQT_output.LR_SR.Rds"))
