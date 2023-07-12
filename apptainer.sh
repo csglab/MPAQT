594f1f1e544b63eda6f574c080925f535dfa64c6
Michael Apostolides <michael.j.apostolides@gmail.com>
2023-07-12 12:46:54 -0400

    starting generate_p.Docker.sh

---
+++ apptainer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load apptainer/1.1.6
+
+singularity shell -B /home \
+                  -B /tmp \
+                  -B /project/6007998/maposto/reference \
+                  -B /project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT \
+                     /project/6007998/maposto/MODULES/MPAQT.simg
+
+singularity exec -B /home \
+                  -B /tmp \
+                  -B /project/6007998/maposto/reference \
+                  -B /project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT \
+                     /project/6007998/maposto/MODULES/MPAQT.simg \
+		     bash TOP_generate_p.Docker.sh
+# singularity exec -B /home -B /absolute/path/to/MetaFusion -B /tmp -B /local/path/to/tmp  /absolute/path/to/MetaFusion.simg bash RUN_MetaFusion.Singularity.sh
