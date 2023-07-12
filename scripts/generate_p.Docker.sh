690558311604b44100a28e56173cc5083d80fa5f
Michael Apostolides <michael.j.apostolides@gmail.com>
2023-07-12 12:46:54 -0400

    starting generate_p.Docker.sh

--- scripts/generate_p.Docker.sh
+++ scripts/generate_p.Docker.sh
@@ -4,14 +4,16 @@
 #topdir=/home/maposto/scratch/pmat_single.TEST_github.JULY-3-2023
 #topdir="/MPAQT/RUNS/P_mat_gen.TEST.July-7-2023"
 #topdir="/MPAQT/RUNS/P_mat_gen.TEST.July-10-2023"
-topdir="/MPAQT/RUNS/P_mat_gen.TEST2.July-10-2023"
+#topdir="/MPAQT/RUNS/P_mat_gen.TEST2.July-10-2023"
+topdir="/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/RUNS/P_mat_gen.cluster.July-11-2023"
 mkdir -p $topdir
 
 mode=single
 #mode=paired
 
-ref_txome=/reference/gencode.v38.transcripts.fa
-KALLISTO_IDX=/reference/gencode.v38.transcripts.fa.idx
+#ref_txome=/reference/gencode.v38.transcripts.fa
+ref_txome=/project/6007998/maposto/reference/kallisto/gencode.v38.transcripts.fa
+KALLISTO_IDX=/project/6007998/maposto/reference/kallisto/gencode.v38.transcripts.fa.idx
 
 #reps=24
 reps=2
