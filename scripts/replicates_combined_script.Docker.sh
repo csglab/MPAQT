d4d3030327f51190dc0323a1613b21676ec92318
Michael Apostolides <michael.j.apostolides@gmail.com>
2023-07-17 15:24:02 -0400

    pmat gen works inside MPAQT.V2.simg with script generate_p.Docker.sh

--- scripts/replicates_combined_script.Docker.sh
+++ scripts/replicates_combined_script.Docker.sh
@@ -30,6 +30,10 @@ while test $# -gt 0;do
         KALLISTO_IDX="${1#*=}"
         shift
         ;;
+        --scripts=*)
+        scripts="${1#*=}"
+        shift
+        ;;
         #*)
         #OTHER_ARGUMENTS+=("$1")
         #shift # Remove generic argument from processing
@@ -50,7 +54,6 @@ echo
 # Arguments
 OUTPUT_DIR=$topdir/rep$rep
 sample=SIM.$rep
-scripts=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/scripts
 
 # Generate fastqs
 echo Generating fastq number $rep
@@ -60,24 +63,25 @@ mkdir -p $OUTPUT_DIR
 cd $OUTPUT_DIR
 Rscript $scripts/simReads.R --rep=$rep --topdir=$OUTPUT_DIR --sample=$sample --ref_txome=$ref_txome --mode=$mode
 
-## Run kallisto bus
-#echo Run kallisto bus $rep
-#FASTQ1=$OUTPUT_DIR/$sample\_R1.fastq.gz
-#
-#echo single end kallisto bus
-#kallisto bus --num -o $OUTPUT_DIR -i  $KALLISTO_IDX $FASTQ1
-#echo $FASTQ1
-#
-## Bustools
-#echo bustools text
-#bustools text -f -o $OUTPUT_DIR/output.bus.txt $OUTPUT_DIR/output.bus
-#
-## Parse fastq read names
-#echo Parse fastq read names
-#cat $OUTPUT_DIR/$sample\_R1.fastq.gz | gunzip  | grep ^@R  | awk  '{gsub("\\|","\t",$0); print;}' |awk  '{gsub(":","\t",$0); print;}' | cut -f 1,2 > $OUTPUT_DIR/read_names.txt
-#
-## Count reads per-EC
-#echo Count reads per-EC
-#Rscript p_matrix_counts.R --rep=$rep --topdir=$OUTPUT_DIR
-#
-#echo "ENDING: " $(date)
+# Run kallisto bus
+echo Run kallisto bus $rep
+FASTQ1=$OUTPUT_DIR/$sample\_R1.fastq.gz
+
+echo single end kallisto bus
+kallisto bus --num -o $OUTPUT_DIR -i  $KALLISTO_IDX $FASTQ1
+echo $FASTQ1
+
+# Bustools
+echo bustools text
+bustools text -f -o $OUTPUT_DIR/output.bus.txt $OUTPUT_DIR/output.bus
+
+# Parse fastq read names
+echo Parse fastq read names
+cat $OUTPUT_DIR/$sample\_R1.fastq.gz | gunzip  | grep ^@R  | awk  '{gsub("\\|","\t",$0); print;}' |awk  '{gsub(":","\t",$0); print;}' | cut -f 1,2 > $OUTPUT_DIR/read_names.txt
+
+# Count reads per-EC
+echo Count reads per-EC
+echo Rscript p_matrix_counts.R --rep=$rep --topdir=$OUTPUT_DIR
+Rscript $scripts/p_matrix_counts.R --rep=$rep --topdir=$OUTPUT_DIR
+
+echo "ENDING: " $(date)
