67e1d1e13d4a1f2bbc9978316d7f1d96ec060f55
Michael Apostolides <michael.j.apostolides@gmail.com>
2023-07-20 09:07:34 -0400

    bash pipeline working with simg

--- scripts/TOP_generate_p.Singularity.sh
+++ scripts/TOP_generate_p.Singularity.sh
@@ -5,7 +5,8 @@
 #topdir=/home/maposto/scratch/pmat_single.TEST_github.JULY-7-2023
 #topdir=/home/maposto/scratch/pmat.TEST_singularity.JULY-12-2023
 #topdir=/scratch/maposto/pmat.TEST_singularity.JULY-13-2023
-topdir=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/RUNS/pmat.TEST_singularity.JULY-19-2023
+#topdir=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/RUNS/pmat.TEST_singularity.JULY-19-2023
+topdir=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/RUNS/pmat_sing.JULY-19-2023
 mkdir -p $topdir
 
 
@@ -14,13 +15,16 @@ KALLISTO_IDX=/project/6007998/maposto/reference/kallisto/gencode.v38.transcripts
 scripts=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/scripts
 lib_size=10000
 mode=single
+reps=2
 #mode=paired
 
 # REPLICATES
 #sbatch --job-name="P_mat_replicates" --cpus-per-task=1 --time=4:00:00 --mem=40G --array=1-2 --err=logs/err.%x.%j.log --output=logs/out.%x.%j.log --account=rrg-hsn \
 
 ## THIS ONE:
-#sbatch --export=topdir=$topdir,mode=$mode,ref_txome=$ref_txome,KALLISTO_IDX=$KALLISTO_IDX,lib_size=$lib_size,scripts=$scripts --job-name="P_mat_replicates" --cpus-per-task=1 --time=0:20:00 --mem=10G --array=1-2 --err=logs/err.%x.%j.log --output=logs/out.%x.%j.log --account=rrg-hsn tmp.sh
+job1=$(sbatch --export=topdir=$topdir,mode=$mode,ref_txome=$ref_txome,KALLISTO_IDX=$KALLISTO_IDX,lib_size=$lib_size,scripts=$scripts --job-name="P_mat_replicates" --cpus-per-task=1 --time=0:20:00 --mem=10G --array=1-2 --err=logs/err.%x.%j.log --output=logs/out.%x.%j.log --account=rrg-hsn tmp.sh)
+echo $job1
+job1=$(echo $job1 | awk '{print $NF}')
  
 #job1=$(sbatch replicates_combined_script.Docker.sh --rep=$rep  --topdir=$topdir --mode=$mode --ref_txome=$ref_txome --KALLISTO_IDX=$KALLISTO_IDX)
 #echo $job1
@@ -30,5 +34,5 @@ mode=single
 #job2=$(sbatch --account=rrg-hsn --dependency=afterany:$job1 --export=topdir=$topdir,env=$env RUN_combine_replicate_counts.sh) 
 #job2=$(sbatch --account=rrg-hsn --export=topdir=$topdir RUN_combine_replicate_counts.sh) 
 
-sbatch --job-name="combine_replicate_counts" --output=logs/out.%x.%j.log --error=logs/err.%x.%j.log --time=0:20:00 --mem=10G --account=rrg-hsn --export=topdir=$topdir tmp2.sh 
-#echo $job2
+job2=$(sbatch --dependency=afterany:$job1 --job-name="combine_replicate_counts" --output=logs/out.%x.%j.log --error=logs/err.%x.%j.log --time=0:20:00 --mem=10G --account=rrg-hsn --export=topdir=$topdir,reps=$reps,scripts=$scripts tmp2.sh) 
+echo $job2
