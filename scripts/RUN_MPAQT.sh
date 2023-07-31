0c295dae020e48efb5c1becd6004821e1f3a8ac8
Michael Apostolides <maposto@Michaels-MacBook-Pro.local>
2023-07-31 16:04:55 -0400

    wrapper scripts and singularity exec RUN_MPAQT.sh

--- scripts/RUN_MPAQT.sh
+++ scripts/RUN_MPAQT.sh
@@ -20,19 +20,24 @@ sample=SOX10_Day61.rep1
 OUTPUT_DIR=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/RUNS/MPAQT_test.FULL
 mkdir $OUTPUT_DIR
 
-refdir=/project/6007998/maposto
-p_list=$refdir/reference/P/p_list.pmat_single.2.4_billion.Oct-15-2021.Rds
-p_rowSums=$refdir/reference/P/p_rowSums.pmat_single.2.4_billion.Oct-15-2021.Rds
-covMx=$refdir/reference/covMx.Rds
-KALLISTO_IDX=/project/6007998/maposto/reference/kallisto/gencode.v38.transcripts.fa.idx
+refdir=/project/6007998/maposto/reference
+p_list=$refdir/P/p_list.pmat_single.2.4_billion.Oct-15-2021.Rds
+p_rowSums=$refdir/P/p_rowSums.pmat_single.2.4_billion.Oct-15-2021.Rds
+covMx=$refdir/covMx.Rds
+KALLISTO_IDX=$refdir/kallisto/gencode.v38.transcripts.fa.idx
 scripts=/project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT/scripts
 
-bash MPAQT.sh --scripts=$scripts \
+# singularity exec -B /home -B /tmp -B /project/6007998/maposto/reference -B /project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT -B /project/6007998/maposto/PROJECTS/neurondiff/20220415_neurondiff_RNAseq/ /project/6007998/maposto/MODULES/MPAQT.V2.simg bash RUN_MPAQT.sh
+singularity exec -B $refdir \
+                 -B /project/6007998/maposto/PROJECTS/MPAQT_FINAL/MPAQT \
+                 -B /project/6007998/maposto/PROJECTS/neurondiff/20220415_neurondiff_RNAseq/ \
+                 /project/6007998/maposto/MODULES/MPAQT.V2.simg \
+                 bash MPAQT.sh --scripts=$scripts \
               --KALLISTO_IDX=$KALLISTO_IDX \
-	      --p_list=$p_list \
-	      --p_rowSums=$p_rowSums \
-	      --covMx=$covMx \
-	      --sample=$sample \
-	      --FASTQ1=$FASTQ1 \
-	      --FASTQ2=$FASTQ2 \
-	      --OUTPUT_DIR=$OUTPUT_DIR
+              --p_list=$p_list \
+	          --p_rowSums=$p_rowSums \
+	          --covMx=$covMx \
+	          --sample=$sample \
+	          --FASTQ1=$FASTQ1 \
+	          --FASTQ2=$FASTQ2 \
+	          --OUTPUT_DIR=$OUTPUT_DIR
